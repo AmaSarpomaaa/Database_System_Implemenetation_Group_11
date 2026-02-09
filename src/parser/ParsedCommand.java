3c7943016ba4ca976f6f092eaa893f1e66f11b25
Javin Parker <jmp2529@rit.edu>
2026-02-09 16:12:54 -0500

    Created added constructor and getters for ParsedCommand class

--- src/parser/ParsedCommand.java
+++ src/parser/ParsedCommand.java
@@ -1,15 +1,43 @@
 package parser;
 
 import java.util.List;
-import java.util.concurrent.locks.Condition;
 
 public class ParsedCommand {
 
-    private CommandType type;
-    private List<String> tableNames;
-    private List<String> attributes;
-    private List<Object> values;
-    private Condition conditions;
-    private String orderBy;
+    private final CommandType type;
+    private final List<String> tableNames;
+    private final List<String> attributes;
+    private final List<Object> values;
+    private final String orderBy;
+
+    public ParsedCommand(CommandType type, List<String> tableNames, List<String> attributes, List<Object> values, String orderBy)
+    {
+        this.type = type;
+        this.tableNames = tableNames;
+        this.attributes = attributes;
+        this.values = values;
+//        this.conditions = conditions;
+        this.orderBy = orderBy;
+    }
+
+    public CommandType getType() {
+        return type;
+    }
+
+    public List<String> getTableNames() {
+        return tableNames;
+    }
+
+    public List<String> getAttributes() {
+        return attributes;
+    }
+
+    public List<Object> getValues() {
+        return values;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
 
 }
