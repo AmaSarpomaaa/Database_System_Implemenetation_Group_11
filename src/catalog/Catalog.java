e872c35693b10e6de418caab555a56d8ab8cd4b4
Garrett Mihalitsas <gem4775@g.rit.edu>
2026-02-15 17:40:44 -0500

    added a few methods

--- src/catalog/Catalog.java
+++ src/catalog/Catalog.java
@@ -1,19 +1,43 @@
 package catalog;
 
+import model.Schema;
 import model.Table;
 import util.DBException;
 
-public interface Catalog {
+import java.io.File;
+import java.util.Map;
 
-    void load() throws DBException;
+public class Catalog implements CatalogIN {
+    Map<String, Table> tables;
+    private final int page_size;
+    public Catalog(int page_size){
+        this.page_size = page_size;
+    }
 
-    void save() throws DBException;
+    public void load() throws DBException{
+        
+    }
 
-    void addTable(Table table) throws DBException;
+    public void save() throws DBException{
 
-    void removeTable(String tableName) throws DBException;
+    }
 
-    Table getTable(String tableName) throws DBException;
+    public void addTable(Table table) throws DBException{
 
-    boolean exists(String tableName);
+    }
+
+    public void removeTable(String tableName) throws DBException{
+
+    }
+
+    public Table getTable(String tableName) throws DBException{
+        return null;
+    }
+
+    public boolean exists(String tableName){
+        return false;
+    }
+    public int getPage_size() {
+        return page_size;
+    }
 }
