4681be6311e101df5fef7969a2e3002e267c13c5
Javin Parker <jmp2529@rit.edu>
2026-02-17 22:30:23 -0500

    Created AlterTableCommand abstract class.

---
+++ src/model/AlterTableCommand.java
@@ -0,0 +1,44 @@
+package model;
+
+import parser.CommandType;
+
+/**
+ * An abstract class to represent a ParsedCommand for an Alter statement.<br>
+ * ParsedCommands that are found to be AlterTableCommands should use isAdd() or
+ * isDrop() to determine whether to use the AlterTableAddCommand or
+ * AlterTableDropCommand subclass to process the command.<br>
+ * Subclasses that extend this class should return true in all cases for
+ * exactly one of isAdd() and isDrop().
+ */
+public abstract class AlterTableCommand extends ParsedCommand {
+
+    private final String tableName;
+
+    public AlterTableCommand(String tableName) {
+        this.tableName = tableName;
+    }
+
+    @Override
+    public CommandType getType() {
+        return CommandType.ALTER;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    /**
+     * Exactly one of isAdd and isDrop should return true for any subclass of
+     * AlterTableCommand.
+     * @return true if the command is an ADD command, false otherwise
+     */
+    public abstract boolean isAdd();
+
+    /**
+     * Exactly one of isAdd and isDrop should return true for any subclass of
+     * AlterTableCommand.
+     * @return true if the command is a DROP command, false otherwise
+     */
+    public abstract boolean isDrop();
+
+}
