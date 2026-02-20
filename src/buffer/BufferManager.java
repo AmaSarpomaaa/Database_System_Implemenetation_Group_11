45c618f1d11a5c9384389be89ef4c9c07e9a1842
MatthewA570 <maa3833@rit.edu>
2026-02-20 17:33:55 -0500

    serialization and deserialization of Record and added documentation

--- src/buffer/BufferManager.java
+++ src/buffer/BufferManager.java
@@ -8,7 +8,7 @@ import java.util.*;
 import java.io.*;
 import java.nio.ByteBuffer;
 
-public class BufferManager {
+public class BufferManager{
     private int maxBufferSize;
     private int pageSize;
     private StorageManager storage;
@@ -17,12 +17,12 @@ public class BufferManager {
     private Set<Integer> dirtyPages;
 
     /**
-     *
-     * @param capacity
-     * @param pageSize
-     * @param storage
+     *  Creates a new instance of a Buffer Manager
+     * @param capacity the max buffer size intended
+     * @param pageSize provided size of Page object
+     * @param storage instance of StorageManager
      */
-    public void initialize(int capacity, int pageSize, StorageManager storage) {
+    public void initialize(int capacity, int pageSize, StorageManager storage){
         this.maxBufferSize = capacity;
         this.pageSize = pageSize;
         this.storage = storage;
@@ -37,7 +37,7 @@ public class BufferManager {
      * @param pageId The unique identifier of a Page
      * @return The requested Page object associated with given pageId
      */
-    public Page getPage(int pageId) throws DBException {
+    public Page getPage(int pageId) throws DBException{
         // Check if data is already in RAM
         if(bufferPool.containsKey(pageId)){
             updateLRU(pageId); // Marks data as recently used to avoid eviction
@@ -71,10 +71,10 @@ public class BufferManager {
 
     /**
      * Marks a page as modified/dirty within the buffer's dirty pages collection
-     * @param pageId
-     * @throws DBException
+     * @param pageId int
+     * @throws DBException aa
      */
-    public void markDirty(int pageId) throws DBException {
+    public void markDirty(int pageId) throws DBException{
         // Look for Page associated within bufferPool. Add to dirtyPages
         if(bufferPool.containsKey(pageId)){
             dirtyPages.add(pageId);
@@ -83,9 +83,9 @@ public class BufferManager {
 
     /**
      * Clears all collections
-     * @throws DBException
+     * @throws DBException don't know
      */
-    public void flushAll() throws DBException {
+    public void flushAll() throws DBException{
         for(Integer pageId: bufferPool.keySet()){
             // Iterate through dirty pages to save all modified
             if(dirtyPages.contains(pageId)){
@@ -103,15 +103,15 @@ public class BufferManager {
     /**
      * Removes the last recently used element within the lruTracker
      * Checks if it is within the dirty pages as well
-     * @throws DBException
+     * @throws DBException again dk why this would throw exception
      */
-    public void evictIfNeeded() throws DBException {
-        if (bufferPool.size() >= maxBufferSize) {
+    public void evictIfNeeded() throws DBException{
+        if (bufferPool.size() >= maxBufferSize){
             // Get rid of the oldest used elem
             int targetId = lruTracker.removeLast();
             Page targetPage = bufferPool.get(targetId);
             // Check if it is modified or dirty
-            if (dirtyPages.contains(targetId)) {
+            if (dirtyPages.contains(targetId)){
                 byte[] data = serializePage(targetPage);
                 storage.writePageBytes(targetId, data);
                 dirtyPages.remove(targetId);
@@ -121,44 +121,53 @@ public class BufferManager {
         }
     }
 
-    private byte[] serializePage(Page page) {
+    /**
+     * Converts a Page object into a corresponding byte[]
+     * @param page provided Page
+     * @return byte[] representation of a Page
+     */
+    private byte[] serializePage(Page page){
         byte[] data = new byte[pageSize];
-        ByteBuffer buffer = ByteBuffer.wrap(data);
+        ByteBuffer bufferz = ByteBuffer.wrap(data);
 
         List<Record> records = page.getRecords();
         int numRecords = records.size();
 
-        buffer.putInt(0, numRecords);
-
+        bufferz.putInt(0, numRecords);
         int currentOffset = pageSize;
-
-        for (int i = 0; i < numRecords; i++) {
+        // Iterate through number of Records in Page, converting to bytes, and placing them withiin the bytebuffer
+        for (int i = 0; i < numRecords; i++){
             Record rec = records.get(i);
             byte[] recBytes = serializeRecord(rec);
-
             currentOffset = currentOffset - recBytes.length;
 
-            for(int j = 0; j < recBytes.length; j++) {
+            // Calculation of offset serialize
+            for(int j = 0; j < recBytes.length; j++){
                 data[currentOffset + j] = recBytes[j];
             }
-
             int headerPos = 4 + (i * 4);
-            buffer.putInt(headerPos, currentOffset);
+            bufferz.putInt(headerPos, currentOffset);
         }
 
         return data;
     }
 
-    private Page deserializePage(int pageId, byte[] data) {
+    /**
+     *  Converts a byte collection into a Page object
+     * @param pageId the identification number of a Page
+     * @param data byte collection
+     * @return Page object
+     */
+    private Page deserializePage(int pageId, byte[] data){
         Page page = new Page(pageId);
-        ByteBuffer buffer = ByteBuffer.wrap(data);
+        ByteBuffer bufferz = ByteBuffer.wrap(data);
 
-        int numRecords = buffer.getInt(0);
-
-        for (int i = 0; i < numRecords; i++) {
+        int numRecords = bufferz.getInt(0);
+        // Iterate through numRecords adding associated Records to Page
+        for (int i = 0; i < numRecords; i++){
+            // Modify header position and calculate offset
             int headerPos = 4 + (i * 4);
-            int offset = buffer.getInt(headerPos);
-
+            int offset = bufferz.getInt(headerPos);
             Record rec = deserializeRecord(data, offset);
             page.addRecord(rec);
         }
@@ -166,11 +175,101 @@ public class BufferManager {
         return page;
     }
 
-    private byte[] serializeRecord(Record rec) {
-        return new byte[0];
+    /**
+     * Allows for the transformation of a Record object into a byte[] needed to
+     * send and receive data as bytes
+     * @param rec Record
+     * @return byte collection representation of provided Record
+     */
+    private byte[] serializeRecord(Record rec){
+        // Extract attributes from given record
+        List<Object> attributes = rec.getAttributes();
+        // counter for number of bytes needed to store complete record in a byte[]
+        int totSize = 4;
+
+        // Iterate through number of attributes
+        for (int i = 0; i < attributes.size(); i++){
+            // Check type of attribute to get needed size for bytebuffer (String, Double, Integer)
+            // num of bytes needed for type of attribute
+            Object obj = attributes.get(i);
+            totSize = totSize + 1;
+            if (obj instanceof Integer){
+                totSize = totSize + 4;
+            }
+            else if (obj instanceof Double){
+                totSize = totSize + 8;
+            }
+            else if (obj instanceof String){
+                String s = (String) obj;
+                totSize = totSize + 4 + s.length();
+            }
+        }
+        // Create a byte[] with the calculated total size of attributes
+        byte[] recData = new byte[totSize];
+        ByteBuffer bufferz = ByteBuffer.wrap(recData);
+        bufferz.putInt(attributes.size());
+        // Iterate through all attributes, placing each into the empty byteBuffer
+        // Type dependent with size
+        // An integer [1, 2, 3] is placed within the byteBuffer which indicates the type of the attribute
+        for (int i = 0; i < attributes.size(); i++){
+            Object obj = attributes.get(i);
+            if (obj instanceof Integer){
+                bufferz.put((byte) 1);
+                bufferz.putInt((Integer) obj);
+            }
+            else if (obj instanceof Double){
+                bufferz.put((byte) 2);
+                bufferz.putDouble((Double) obj);
+            }
+            else if (obj instanceof String){
+                bufferz.put((byte) 3);
+                String st = (String) obj;
+                bufferz.putInt(st.length());
+                for (int j = 0; j < st.length(); j++){
+                    bufferz.put((byte) st.charAt(j));
+                }
+            }
+        }
+        return recData;
     }
 
-    private Record deserializeRecord(byte[] data, int offset) {
-        return new Record();
+    /**
+     * Converts a collection of bytes into a Record object using a given offset
+     * @param data byte collection
+     * @param offset int
+     * @return Record object
+     */
+    private Record deserializeRecord(byte[] data, int offset){
+        Record rec = new Record();
+        ByteBuffer bufferz = ByteBuffer.wrap(data);
+        bufferz.position(offset);
+
+        int numAttributes = bufferz.getInt();
+
+        // Iterate through number of attributes
+        for (int i = 0; i < numAttributes; i++){
+            // The byte buffer contains a number indicating the type of the attribute. A type tag. Using this attribute, we can
+            // get the type and correspondingly convert it
+            byte type = bufferz.get();
+
+            if (type == 1){
+                int val = bufferz.getInt();
+                rec.addAttribute(val);
+            }
+            else if (type == 2){
+                double val = bufferz.getDouble();
+                rec.addAttribute(val);
+            }
+            else if (type == 3){
+                int len = bufferz.getInt();
+                byte[] strBytes = new byte[len];
+                for (int j = 0; j < len; j++){
+                    strBytes[j] = bufferz.get();
+                }
+                String val = new String(strBytes);
+                rec.addAttribute(val);
+            }
+        }
+        return rec;
     }
 }
