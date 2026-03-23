e5d4ac37de6f07458f106e25c47b68c131a79326
matthew ayala <matthewayala@matthews-MacBook-Air.local>
2026-03-23 11:13:43 -0400

    OR tree class and constructor created

---
+++ src/parser/ORTree.java
@@ -0,0 +1,20 @@
+package parser;
+
+import model.Record;
+import model.Schema;
+import util.DBException;
+
+public class ORTree implements IWhereTree{
+    private IWhereTree leftN;
+    private IWhereTree rightN;
+
+    public ORTree(IWhereTree leftN, IWhereTree rightN){
+        this.leftN = leftN;
+        this.rightN = rightN;
+    }
+
+    @Override
+    public boolean evaluate(Schema scheme, Record record) throws DBException {
+        return false;
+    }
+}
