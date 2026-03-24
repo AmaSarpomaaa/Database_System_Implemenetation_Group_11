9909350aaa83e6dd575a53bb06168b496c96d1b3
Tristan Loutan <tll1477@g.rit.edu>
2026-03-23 22:49:04 -0400

    from implemented

--- src/model/SelectCommand.java
+++ src/model/SelectCommand.java
@@ -1,6 +1,10 @@
 package model;
 
 import parser.CommandType;
+import catalog.FileCatalog;
+import util.DBException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A class to represent a ParsedCommand for a Select statement.
@@ -29,6 +33,50 @@ public abstract class SelectCommand extends ParsedCommand {
         return tableNames;
     }
 
+    public Table from(FileCatalog catalog) throws DBException {
+        if (tableNames.length == 1) {
+            return catalog.getTable(tableNames[0]);
+        }
+
+        // Start with the first table
+        Table result = catalog.getTable(tableNames[0]);
+
+        // Join with each next table
+        for (int i = 1; i < tableNames.length; i++) {
+            Table right = catalog.getTable(tableNames[i]);
+            result = cartesianProduct(result, right);
+        }
+
+        return result;
+    }
+
+    /**
+     * Creates a new temporary in-memory table that is the Cartesian
+     * product of left and right
+     */
+    private Table cartesianProduct(Table left, Table right) throws DBException {
+        List<Attribute> mergedAttrs = new ArrayList<>();
+        for (Attribute a : left.schema().getAttributes()) {
+            mergedAttrs.add(new Attribute(left.name() + "." + a.getName(), false, false, a.getType(), a.getDataLength()));
+        }
+        for (Attribute a : right.schema().getAttributes()) {
+            mergedAttrs.add(new Attribute(right.name() + "." + a.getName(), false, false, a.getType(), a.getDataLength()));
+        }
+
+        Schema mergedSchema = new Schema(mergedAttrs);
+        TempTable temp = new TempTable(left.name() + "_" + right.name(), mergedSchema);
+
+        for (Record leftRec : left.scan()) {
+            for (Record rightRec : right.scan()) {
+                Record combined = new Record();
+                for (Value v : leftRec.getAttributes()) combined.addAttribute(v);
+                for (Value v : rightRec.getAttributes()) combined.addAttribute(v);
+                temp.insert(combined);
+            }
+        }
+
+        return temp;
+    }
 }
 
 /*
