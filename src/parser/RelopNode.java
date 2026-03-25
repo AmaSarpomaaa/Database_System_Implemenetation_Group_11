eb4d4072a463a69458c565e70755f2147b7251a3
MatthewA570 <maa3833@rit.edu>
2026-03-25 00:51:45 -0400

    RelopNode class completed with constructor and evaluate functions implemented.

--- src/parser/RelopNode.java
+++ src/parser/RelopNode.java
@@ -2,17 +2,58 @@ package parser;
 
 import model.Record;
 import model.Schema;
+import model.Value;
 import util.DBException;
 
 public class RelopNode implements IWhereTree{
     private IOperandNode left;
     private IOperandNode right;
+    private String operator;
 
 
+    public RelopNode(IOperandNode left, IOperandNode right, String operator){
+        this.left = left;
+        this.right = right;
+        this.operator = operator;
+    }
+
+    @SuppressWarnings("all")
     @Override
     public boolean evaluate(Schema scheme, Record record) throws DBException {
-//        Check type if value or attribute node
-//
-        return false;
+        //TODO: check if this line works or if I have to separate it into two variables
+        Object leftValRaw= (left.getVal(scheme, record)).getRaw();
+        Object rightValRaw= (right.getVal(scheme, record)).getRaw();
+
+        //TODO: check for null values
+        if(leftValRaw == null || rightValRaw == null){
+            throw new DBException("NULL values can not be evaluated");
+        }
+        if(leftValRaw.getClass() != rightValRaw.getClass()){
+            throw new DBException("Left node and right node are not of the same type");
+        }
+
+        Comparable<Object> compRight = (Comparable<Object>) rightValRaw;
+        int comparable = compRight.compareTo(leftValRaw);
+        if(operator.equals("==")){
+            return comparable == 0;
+        }
+        if(operator.equals("<>")){
+            return comparable != 0;
+        }
+        if(operator.equals("<")){
+            return comparable < 0;
+        }
+        if(operator.equals(">")){
+            return comparable > 0;
+        }
+        if(operator.equals("<=")){
+            return comparable <= 0;
+        }
+        if(operator.equals(">=")){
+            return comparable >= 0;
+        }
+        else{
+            throw new DBException("Operator provided is invalid:" + operator);
+        }
     }
 }
