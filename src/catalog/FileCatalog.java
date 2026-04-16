7af8c511fbc7eb0ec34c592fc00d4eaeaf314e89
Javin Parker <jmp2529@rit.edu>
2026-04-16 10:07:56 -0400

    Updated FileCatalog to read and write the unique constraint to hardware when reading and writing the catalog.

--- src/catalog/FileCatalog.java
+++ src/catalog/FileCatalog.java
@@ -44,10 +44,11 @@ public class FileCatalog implements Catalog {
                     String attrName = in.readUTF();
                     boolean notNull = in.readBoolean();
                     boolean primaryKey = in.readBoolean();
+                    boolean unique = in.readBoolean();
                     Datatype type = Datatype.valueOf(in.readUTF());
                     int dataLength = in.readInt();
 
-                    attrs.add(new Attribute(attrName, notNull, primaryKey, type, dataLength));
+                    attrs.add(new Attribute(attrName, notNull, primaryKey, unique, type, dataLength));
                 }
 
                 Schema schema = new Schema(attrs);
@@ -84,6 +85,7 @@ public class FileCatalog implements Catalog {
                     out.writeUTF(a.getName());
                     out.writeBoolean(a.isNotNull());
                     out.writeBoolean(a.isPrimaryKey());
+                    out.writeBoolean(a.isUnique());
                     out.writeUTF(a.getType().name());
                     out.writeInt(a.getDataLength());
                 }
